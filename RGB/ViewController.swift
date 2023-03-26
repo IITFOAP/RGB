40b4bbcae1de231c46fae8b23ba3aea5641f1bc3
Baranov Roman <palamarmle@gmail.com>
2023-03-26 17:16:48 +0300

    TEST

--- RGB/ViewController.swift
+++ RGB/ViewController.swift
@@ -7,13 +7,63 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
+    
+    @IBOutlet var acceptsСolor: UIView!
+    
+    @IBOutlet var redColor: UILabel!
+    @IBOutlet var greenColor: UILabel!
+    @IBOutlet var blueColor: UILabel!
+    
+    
+    @IBOutlet var redColorContrast: UILabel!
+    @IBOutlet var greenColorContrast: UILabel!
+    @IBOutlet var blueColorContrast: UILabel!
+   
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        acceptsСolor.layer.cornerRadius = 10
+    
+    }
+    
+    
+    @IBAction func passesTheValuesOfTheRedSlider() {
+        redColorContrast.text = redSlider.value.formatted(
+            .number
+            .precision(
+            .fractionLength(2))
+        )
+
+    }
+    
+    @IBAction func passesTheValuesOfTheGreenSlider() {
+        greenColorContrast.text = greenSlider.value.formatted(
+            .number
+            .precision(
+            .fractionLength(2))
+        )
+     
+
     }
 
+    @IBAction func passesTheValuesOfTheBlueSlider() {
+        blueColorContrast.text = blueSlider.value.formatted(
+            .number
+            .precision(
+            .fractionLength(2))
+        )
+    }
+    
+    func changesColorView() {
+       
+    }
+    
 
+    
 }
 
